bfb30b4cf1368484e56f81eb06c468483d6f98e4
Dexter Lowe <feldoh10@gmail.com>
2023-01-02 04:47:56 +0000

    Fix for forbidden or powered down targets

--- 1.4/Source/SpecificWorkJobGiver/JobGiver_SpecificWork.cs
+++ 1.4/Source/SpecificWorkJobGiver/JobGiver_SpecificWork.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using RimWorld;
 using Verse;
 using Verse.AI;
@@ -11,6 +12,7 @@ namespace SpecificWorkJobGiver
 		private WorkGiverDef workGiverDef = null;
 		private bool factionOnly = true;
 		private bool ignoreOtherReservations = false;
+		private Dictionary<string, int> minNextJobTicks = new();
 
 		public override ThinkNode DeepCopy(bool resolve = true)
 		{
@@ -21,20 +23,32 @@ namespace SpecificWorkJobGiver
 			jobGiverSpecificWork.ignoreOtherReservations = ignoreOtherReservations;
 			return jobGiverSpecificWork;
 		}
-
+		
 		protected override Job TryGiveJob(Pawn pawn)
 		{
-			if (!(workGiverDef.Worker is WorkGiver_Scanner worker) || worker.ShouldSkip(pawn) ||
-			    pawn.CurJobDef == JobDefOf.MechCharge) return null;
-
+			int curTicks = Find.TickManager.TicksGame;
+			minNextJobTicks.TryGetValue(pawn.ThingID, out var firstValidTickForJob);
+			
+			if (workGiverDef.Worker is not WorkGiver_Scanner worker ||
+			    worker.ShouldSkip(pawn) ||
+			    pawn.CurJobDef == JobDefOf.MechCharge || firstValidTickForJob > curTicks) return null;
+			
 			bool Validator(Thing x) => (!factionOnly || x.Faction == pawn.Faction) &&
+			                           !x.IsForbidden(pawn) &&
+			                           (x.TryGetComp<CompPowerTrader>()?.PowerOn ?? true) &&
 			                           pawn.CanReserve((LocalTargetInfo)x,
 				                           ignoreOtherReservations: ignoreOtherReservations);
 
 			LocalTargetInfo target = GenClosest.ClosestThingReachable(pawn.PositionHeld, pawn.MapHeld,
 				worker.PotentialWorkThingRequest, worker.PathEndMode, TraverseParms.For(pawn),
 				validator: Validator) ?? worker.PotentialWorkThingsGlobal(pawn)?.Where(Validator).FirstOrFallback();
-			return target == null ? null : worker.JobOnThing(pawn, target.Thing);
+			if (target == null) return null;
+			Job job = worker.JobOnThing(pawn, target.Thing);
+			
+			// Record any time we successfully assign a job so we can detect when we're assigning the same job repeatedly and back off
+			if (job != null) minNextJobTicks[pawn.ThingID] = curTicks + (firstValidTickForJob == curTicks ? 100 : 0);
+
+			return job;
 		}
 
 		public override float GetPriority(Pawn pawn) => overridePriority < 0f ? 9f : overridePriority;
